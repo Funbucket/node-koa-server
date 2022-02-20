1a292bb5d99908e43ed33d60decf48da9fd692e7
Joe <outofchng@gmail.com>
2022-02-20 18:57:31 +0900

    deploy test

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ const { getUsers, getUser } = require('../controller');
 
 const router = new Router();
 
-router.get('/', (ctx) => ctx.body = 'Hello world !');
+router.get('/', (ctx) => ctx.body = 'Hello world!!');
 
 router.get('/users', getUsers);
 router.get('/users/:id', getUser);
